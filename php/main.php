91819d40a3f479c740a55b354b92d8ec5da808ba
danielmbs77 <danielmbs76@outlook.com>
2018-04-20 18:32:56 -0300

    Pagina principal basica criada

---
+++ php/main.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<!-- Definicoes do header - BEGIN -->
+	<title> IoL - Igreja On Line - Pagina Principal </title>	
+	<meta charset="utf-8" />
+	<!-- Definicoes do header - END -->
+	
+	<!-- Inclusao de scripts e CSS -->
+	<link href="css/main.css" rel="stylesheet" media="all"/>
+	<script src="js/scripts.js"></script>
+</head>
+<body>
+	<!-- <p> IoL - Igreja On Line </p> -->
+	<p> IoL - Igreja On Line - Pagina Principal </p> 
+</body>
+</html>
